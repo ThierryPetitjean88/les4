07a27af4dea3913d6c67bf85fea8f7a240f6eeeb
Petitjean <tpetitjean@mborijnland.nl>
2025-03-18 11:05:24 +0100

    Added index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>023402340234</h1>
+    </header>   
+    <main>  
+        <p>Thierry Petitjean</p>   
+    </main>
+    <footer>
+        <p>SWD2</p>   
+    </footer>
+</body>
+</html>
